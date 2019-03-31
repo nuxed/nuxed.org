dceee358cec107998fa36c1b4e8c68b7ccc9452f
Baddin <baddin2016@gmail.com>
2019-04-01 00:22:39 +0100

    Updates

---
+++ precache-manifest.a9c02410bb10f6cccaad3cf0aaab9737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a12de0c4e0b4350f460e",
+    "url": "/static/css/main.d49a28b3.chunk.css"
+  },
+  {
+    "revision": "a12de0c4e0b4350f460e",
+    "url": "/static/js/main.dd5895b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "66ec71a1893c7d2b62bf4ba788568945",
+    "url": "/index.html"
+  }
+];
