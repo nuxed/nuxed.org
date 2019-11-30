6cf4a38365f3175cc78d7c7dcef5b704b142158f
azjezz <azjezz@protonmail.com>
2019-11-30 01:34:27 +0100

    setup a simple "coming soon" page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" alt="Nuxed logo" src="./assets/logo.svg">
+    <h1>Coming soon!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -24,5 +19,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  max-width: 10rem;
+  margin-bottom: 5rem;
 }
 </style>
